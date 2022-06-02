b27e23c0b0aebdfd159d9b4c047fe68ad4aa8699
Jacieldo <jacieldo@hotmail.com>
2022-06-02 17:59:15 -0300

    UpDate 3
    Trabalhando imagens

---
+++ JosiArq/script.js
@@ -0,0 +1,26 @@
+let time = 2000,
+    currentImageIndex = 0,
+    imagens = document.querySelectorAll("#slider img")
+    max = imagens.length;
+
+
+function nextImage() {
+
+    imagens[currentImageIndex].classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex =0
+
+    imagens[currentImageIndex].classList.add("selected")
+}
+function start() {
+    setInterval(() => {
+        //troca de imagens
+
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
