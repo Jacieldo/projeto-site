289f40344a602c0d0a20ef8872e3e6403f8c0a58
Jacieldo <jacieldo@hotmail.com>
2022-06-27 03:26:13 -0300

    Definição do menu-navBar
    Apresentação para possíveis mudanças

--- JosiArq/script.js
+++ JosiArq/script.js
@@ -1,26 +1,44 @@
-let time = 2000,
-    currentImageIndex = 0,
-    imagens = document.querySelectorAll("#slider img")
-    max = imagens.length;
-
-
-function nextImage() {
-
-    imagens[currentImageIndex].classList.remove("selected")
-
-    currentImageIndex++
-
-    if(currentImageIndex >= max)
-        currentImageIndex =0
-
-    imagens[currentImageIndex].classList.add("selected")
-}
-function start() {
-    setInterval(() => {
-        //troca de imagens
-
-        nextImage()
-    }, time)
-}
-
-window.addEventListener("load", start)
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+      this.mobileMenu = document.querySelector(mobileMenu);
+      this.navList = document.querySelector(navList);
+      this.navLinks = document.querySelectorAll(navLinks);
+      this.activeClass = "active";
+  
+      this.handleClick = this.handleClick.bind(this);
+    }
+  
+    animateLinks() {
+      this.navLinks.forEach((link, index) => {
+        link.style.animation
+          ? (link.style.animation = "")
+          : (link.style.animation = `navLinkFade 0.5s ease forwards ${
+              index / 7 + 0.3
+            }s`);
+      });
+    }
+  
+    handleClick() {
+      this.navList.classList.toggle(this.activeClass);
+      this.mobileMenu.classList.toggle(this.activeClass);
+      this.animateLinks();
+    }
+  
+    addClickEvent() {
+      this.mobileMenu.addEventListener("click", this.handleClick);
+    }
+  
+    init() {
+      if (this.mobileMenu) {
+        this.addClickEvent();
+      }
+      return this;
+    }
+  }
+  
+  const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+  );
+  mobileNavbar.init();
